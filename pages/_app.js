648f81e670f551ca3e5643910d56379258755af3
Dexter <dexben5@gmail.com>
2026-06-26 13:28:27 -0500

    add vercel analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,9 @@
 import '../styles/global.css';
+import { Analytics } from "@vercel/analytics/next";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <Analytics>
+      <Component {...pageProps} />
+    </Analytics>);
 }
