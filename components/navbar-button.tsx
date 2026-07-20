60a6ae71501c2ee31d1b45df662fde8b386ca182
Dexter <dexben5@gmail.com>
2026-07-19 23:55:44 -0500

    add skeleton navbar with support for project dropdown with dynamic routing as well as an about page

--- components/navbar-button.tsx
+++ components/navbar-button.tsx
@@ -1,19 +1,68 @@
 import Link from "next/link";
+import { useState } from "react";
+
+type DropdownItem = {
+  label: string;
+  slug: string;
+};
+type NavItem = {
+  label: string;
+  href?: string;
+  dropdown?: DropdownItem[];
+};
+
+type NavBarButtonProps = NavItem & {
+  className?: string;
+};
+
+const buttonStyles =
+  "bg-blue-300 rounded-2xl p-5 hover:rounded-4xl opacity-70 hover:opacity-100 transition-all shadow-2xl justify-center items-center";
 
 export default function NavBarButton({
-  children,
-  className,
-}: {
-  children: React.ReactNode;
-  className: string;
-}) {
+  label,
+  href,
+  dropdown,
+}: NavBarButtonProps) {
+  if (dropdown) {
+    return (
+      <DropDownNavButton label={label} items={dropdown}></DropDownNavButton>
+    );
+  }
+  if (!href) return null;
   return (
-    <Link href="/projects">
-      <div
-        className={`bg-blue-300 rounded-2xl p-5 hover:rounded-4xl opacity-70 hover:opacity-100 transition-all shadow-2xl justify-center items-center ${className}`}
-      >
-        {children}
-      </div>
+    <Link href={href}>
+      <div className={buttonStyles}>{label}</div>
     </Link>
+  );
+}
+
+function DropDownNavButton({
+  label,
+  items,
+}: {
+  label: string;
+  items: DropdownItem[];
+}) {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleDropDown = () => setIsOpen(!isOpen);
+  return (
+    <div className="relative">
+      <button onClick={toggleDropDown} className={buttonStyles}>
+        {label}
+      </button>
+      {isOpen && (
+        <div>
+          {items.map((item) => (
+            <Link
+              key={item.slug}
+              href={`/projects/${item.slug}`}
+              className={buttonStyles}
+            >
+              {item.label}
+            </Link>
+          ))}
+        </div>
+      )}
+    </div>
   );
 }
